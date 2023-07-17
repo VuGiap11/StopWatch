78c8f3e36a4da9bcfe64baf22ebdb7c75899b4e7
vu giap <vudinhgiap11@gmail.com>
2023-07-17 12:06:31 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SW stopwatch = new SW();
+            stopwatch.Start();
+
+            SapXep1();
+
+            stopwatch.Stop();
+            Console.WriteLine(" tong thoi gian sap xem la : {0}", stopwatch.GetELapsedTime());
+
+            stopwatch.Start();
+
+            SapXep2();
+
+            stopwatch.Stop();
+            Console.WriteLine(" tong thoi gian sap xem la : {0}", stopwatch.GetELapsedTime());
+
+
+
+        }
+        private static void SapXep1()
+        {
+            int[] arr = new int[100000];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(1000000);
+            }
+            Array.Sort(arr);
+
+        }
+        private static void SapXep2()
+        {
+            List<int> numberS = new List<int>();
+
+            Random random = new Random(); 
+            for (int i = 0; i < 100000; i++)
+            {
+                numberS.Add(random.Next(100000));
+            }   
+
+            numberS.Sort();
+        }
+    }
+}
---
+++ SW.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    internal class SW
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public SW(DateTime startTime, DateTime endTime)
+        {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+        public SW()
+        {
+            startTime = DateTime.Now;
+        }
+
+        public DateTime StartTime { get => startTime;}
+        public DateTime EndTime { get => endTime;}
+        public void Start()
+        {
+            startTime = DateTime.Now;
+        }
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+        }
+        public double GetELapsedTime()
+        {
+            return(endTime - startTime).TotalMilliseconds;
+        }
+    }
+}
